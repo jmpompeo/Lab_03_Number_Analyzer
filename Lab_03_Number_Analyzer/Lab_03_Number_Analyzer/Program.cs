8270d4212e6a11f218f12bc2ca7dd7317030cb3e
Joe <joeypompeo@gmail.com>
2021-01-25 09:11:34 -0500

    Lab_03 Upload without method creation

---
+++ Lab_03_Number_Analyzer/Lab_03_Number_Analyzer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Lab_03_Number_Analyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string userInput2;
+
+            do
+            {
+                Console.WriteLine("Enter an integer between 1 & 100: ");
+                var userInput = Console.ReadLine();
+                int inputNumber = int.Parse(userInput);
+
+                if (inputNumber >= 1 && inputNumber <= 100)
+                {
+                    if (!(inputNumber % 2 == 0))
+                    {
+                        Console.WriteLine(inputNumber + " This number is odd.");
+                    }
+                    else if (inputNumber % 2 == 0 && (inputNumber >= 2 && inputNumber <= 25))
+                    {
+                        Console.WriteLine(inputNumber + " The number is even and less than 25.");
+                    }
+                    else if (inputNumber % 2 == 0 && inputNumber >= 26)
+                    {
+                        Console.WriteLine(inputNumber + " This number is even.");
+                    }
+                    else if ((!(inputNumber % 2 == 0)) && (inputNumber > 60))
+                    {
+                        Console.WriteLine(inputNumber + " This number is odd.");
+                    }
+                }
+                Console.WriteLine("Would you like to continue (y/n)? ");
+                userInput2 = Console.ReadLine();
+
+            } while (userInput2 == "y");
+
+            if (userInput2 == "n")
+            {
+                Console.WriteLine("Thanks for playing, goodbye!");
+            }
+
+        }
+    }
+}
